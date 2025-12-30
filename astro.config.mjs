6ee335b212a5b42349794287a0a7a19e8a4237d6
Smart Ace <132539186+Smart-Ace-Designs@users.noreply.github.com>
2025-12-30 17:40:49 -0600

    Remove redundant font config property

--- astro.config.mjs
+++ astro.config.mjs
@@ -17,7 +17,6 @@ export default defineConfig({
         cssVariable: "--font-overpass",
         weights: [400, 700],
         styles: ["normal"],
-        subsets: ["latin"],
       },
     ],
   },
